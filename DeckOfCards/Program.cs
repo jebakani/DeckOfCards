9dff5e37d52f87fe25400442e1ab8186f305fc81
Jebakani <vjishwarya@gmail.com>
2021-07-06 20:23:05 +0530

    [JEBAKANI] Creating the deck of card

---
+++ DeckOfCards/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Deck of Cards");
+            Console.Read();
+			
+        }
+    }
+}
